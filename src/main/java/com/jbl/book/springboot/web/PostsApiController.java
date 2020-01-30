421c138f969ae3e6f8c0da22c3e9c3ada4f8b0a5
jaeboklee <jabok0703@gmail.com>
2020-01-30 23:06:20 +0900

    4장 등록 까지 진행(layout(footer,header), indexj.js, IndexController)

--- src/main/java/com/jbl/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jbl/book/springboot/web/PostsApiController.java
@@ -14,8 +14,7 @@ public class PostsApiController {
     private final PostsService postsService;
 
 
-    //@PostMapping("/api/v1/posts")
-    @PutMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestsDto requestsDto) {
         return postsService.save(requestsDto);
     }
